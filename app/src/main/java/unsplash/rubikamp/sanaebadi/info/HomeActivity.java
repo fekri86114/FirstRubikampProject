3515aae52ef3072fceda36dcdf4e45864e4b031b
Sana <sanaebadi97@gmail.com>
2021-11-02 21:56:33 +0330

    added home activity_home.xml added some vectors added some colors and shape_example.xml ....

---
+++ app/src/main/java/unsplash/rubikamp/sanaebadi/info/HomeActivity.java
@@ -0,0 +1,14 @@
+package unsplash.rubikamp.sanaebadi.info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
